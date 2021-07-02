49f17b4a9937c4e6d426f85fe61a386b9ae1e026
wilgur513 <wilgur513@naver.com>
2021-07-02 23:58:32 +0900

    콤마로 구분하는 StringSplitter 테스트 작성

--- src/test/java/once/ch2/StringCalculatorTest.java
+++ src/test/java/once/ch2/StringCalculatorTest.java
@@ -3,6 +3,9 @@ package once.ch2;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -29,5 +32,12 @@ public class StringCalculatorTest {
     public void shouldReturnContinuousSum() {
          assertThat(calculator.calculate("1:3,4"), is(10));
          assertThat(calculator.calculate("1,2,3,5:8"), is(32));
+    }
+
+    @Test
+    public void shouldSplitComma() {
+        StringSplitter splitter = new StringSplitter("1,2");
+        List<String> expected = splitter.split();
+        assertThat(expected, is(Arrays.asList("1", "2")));
     }
 }
