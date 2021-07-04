461046fb7f66ef80f9644cf9ca02b51e0049764a
wilgur513 <wilgur513@naver.com>
2021-07-04 10:19:47 +0900

    index.html 응답하기

--- src/main/java/once/ch3/webserver/RequestHandler.java
+++ src/main/java/once/ch3/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package once.ch3.webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +21,15 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            String line = startHeaderLine(in);
+            log.debug("method: {}, url: {}", method(line), url(line));
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+
+            File file = new File("./webapp/" + url(line));
+            log.debug("file: {}", file.getAbsolutePath());
+
+            byte[] body = Files.readAllBytes(file.toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -51,5 +55,18 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String method(String line) throws IOException {
+        return line.split(" ")[0];
+    }
+
+    private String url(String line) throws IOException {
+        return line.split(" ")[1];
+    }
+
+    private String startHeaderLine(InputStream in) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        return reader.readLine();
     }
 }
