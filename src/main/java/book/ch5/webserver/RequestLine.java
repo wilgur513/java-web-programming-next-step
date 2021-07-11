3f11f141de23e5a077ef435c2d960cfd121a44d2
wilgur513 <wilgur513@naver.com>
2021-07-11 23:47:46 +0900

    RequestLineTest 작성

---
+++ src/main/java/book/ch5/webserver/RequestLine.java
@@ -0,0 +1,21 @@
+package book.ch5.webserver;
+
+import java.util.Map;
+
+public class RequestLine {
+    public RequestLine(String line) {
+
+    }
+
+    public String getMethod() {
+        return null;
+    }
+
+    public String getPath() {
+        return null;
+    }
+
+    public Map<String, String> getParams() {
+        return null;
+    }
+}
