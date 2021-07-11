5a98ff47656d679212091d00896413c5dadd95f8
wilgur513 <wilgur513@naver.com>
2021-07-12 00:20:48 +0900

    HttpResponseTest 작성

---
+++ src/main/java/book/ch5/webserver/HttpResponse.java
@@ -0,0 +1,21 @@
+package book.ch5.webserver;
+
+import java.io.OutputStream;
+
+public class HttpResponse {
+    public HttpResponse(OutputStream out) {
+
+    }
+
+    public void forward(String path) {
+
+    }
+
+    public void sendRedirect(String path) {
+
+    }
+
+    public void addHeader(String key, String value) {
+        
+    }
+}
