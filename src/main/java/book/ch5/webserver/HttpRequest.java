0c75d7ad990782af8ac0e954461d7a5de9c0eb96
wilgur513 <wilgur513@naver.com>
2021-07-11 23:15:28 +0900

    HttpRequestTest 작성

---
+++ src/main/java/book/ch5/webserver/HttpRequest.java
@@ -0,0 +1,25 @@
+package book.ch5.webserver;
+
+import java.io.InputStream;
+
+public class HttpRequest {
+    public HttpRequest(InputStream in) {
+
+    }
+
+    public String getMethod() {
+        return null;
+    }
+
+    public String getPath() {
+        return null;
+    }
+
+    public String getHeader(String key) {
+        return null;
+    }
+
+    public String getParameter(String key) {
+        return null;
+    }
+}
---
+++ src/test/java/book/ch5/webserver/HttpRequestTest.java
@@ -0,0 +1,37 @@
+package book.ch5.webserver;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class HttpRequestTest {
+    private String testDir = "./src/test/resources/";
+
+    @Test
+    public void request_GET() throws FileNotFoundException {
+        InputStream in = new FileInputStream(new File(testDir + "Http_GET.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertThat(request.getMethod(), is("GET"));
+        assertThat(request.getPath(), is("/user/create"));
+        assertThat(request.getHeader("Connection"), is("keep-alive"));
+        assertThat(request.getParameter("userId"), is("javajigi"));
+    }
+
+    @Test
+    public void request_POST() throws FileNotFoundException {
+        InputStream in = new FileInputStream(new File(testDir + "Http_GET.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertThat(request.getMethod(), is("POST"));
+        assertThat(request.getPath(), is("/user/create"));
+        assertThat(request.getHeader("Connection"), is("keep-alive"));
+        assertThat(request.getParameter("userId"), is("javajigi"));
+    }
+}
