00d39d3da111a8215f6bef319ed7fa48b7f7699f
wilgur513 <wilgur513@naver.com>
2021-07-14 15:53:17 +0900

    6장 세팅

---
+++ src/main/java/once/ch6/controller/CreateUserServlet.java
@@ -0,0 +1,31 @@
+package once.ch6.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import once.ch6.model.User;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import once.ch6.db.DataBase;
+
+@WebServlet("/user/create")
+public class CreateUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(CreateUserServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+                req.getParameter("email"));
+        log.debug("user : {}", user);
+        DataBase.addUser(user);
+        resp.sendRedirect("/user/list");
+    }
+}
---
+++ src/main/java/once/ch6/db/DataBase.java
@@ -0,0 +1,24 @@
+package once.ch6.db;
+
+import java.util.Collection;
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import once.ch6.model.User;
+
+public class DataBase {
+    private static Map<String, User> users = Maps.newHashMap();
+
+    public static void addUser(User user) {
+        users.put(user.getUserId(), user);
+    }
+
+    public static User findUserById(String userId) {
+        return users.get(userId);
+    }
+
+    public static Collection<User> findAll() {
+        return users.values();
+    }
+}
