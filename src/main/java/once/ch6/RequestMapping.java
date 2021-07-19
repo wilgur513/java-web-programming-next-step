d57e0be1377e6b343234c9fc6f3d874ac5e44baf
wilgur513 <wilgur513@naver.com>
2021-07-19 20:18:31 +0900

    RequestMapping 구현

---
+++ src/main/java/once/ch6/RequestMapping.java
@@ -0,0 +1,23 @@
+package once.ch6;
+
+import once.ch6.controller.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestMapping {
+    private static Map<String, Controller> mapping = new HashMap<>();
+
+    static {
+        mapping.put("/user/create", new CreateUserController());
+        mapping.put("/user/list", new ListUserController());
+        mapping.put("/user/login", new LoginUserController());
+        mapping.put("/user/logout", new LogoutUserController());
+        mapping.put("/user/update", new UserUpdateController());
+        mapping.put("/user/update-form", new UserUpdateFormController());
+    }
+
+    public static Controller find(String url) {
+        return mapping.get(url);
+    }
+}
