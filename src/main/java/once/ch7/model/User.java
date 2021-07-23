d469b910b4b48d8d4c5034226eb06bc66f1fa3f3
wilgur513 <wilgur513@naver.com>
2021-07-24 00:01:08 +0900

    UserDaoTest 통과

--- src/main/java/once/ch7/model/User.java
+++ src/main/java/once/ch7/model/User.java
@@ -75,4 +75,11 @@ public class User {
                 ", email='" + email + '\'' +
                 '}';
     }
+
+    public void update(User user) {
+        this.userId = user.getUserId();
+        this.password = user.getPassword();
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
 }
---
+++ src/test/java/once/ch7/UserDaoTest.java
@@ -0,0 +1,43 @@
+package once.ch7;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import once.ch7.dao.UserDao;
+import once.ch7.model.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+public class UserDaoTest {
+    @Before
+    public void setup() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("once/ch7/jwp.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    public void crud() throws Exception {
+        User expected = new User("userId", "password", "name", "javajigi@email.com");
+        UserDao userDao = new UserDao();
+        userDao.insert(expected);
+        User actual = userDao.findByUserId(expected.getUserId());
+        assertEquals(expected, actual);
+
+        expected.update(new User("userId", "password2", "name2", "sanjigi@email.com"));
+        userDao.update(expected);
+        actual = userDao.findByUserId(expected.getUserId());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        UserDao userDao = new UserDao();
+        List<User> users = userDao.findAll();
+        assertEquals(1, users.size());
+    }
+}
