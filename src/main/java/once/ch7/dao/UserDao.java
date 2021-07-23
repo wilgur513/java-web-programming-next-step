4267f6aa9375dc63d6d51dd29cbab0ca100cef96
wilgur513 <wilgur513@naver.com>
2021-07-24 01:39:53 +0900

    RowMapper에 람다 적용

--- src/main/java/once/ch7/dao/UserDao.java
+++ src/main/java/once/ch7/dao/UserDao.java
@@ -9,43 +9,25 @@ import java.util.List;
 
 public class UserDao {
     public void insert(User user) {
-        JdbcTemplate template = new JdbcTemplate();
-        template.update("INSERT INTO USERS VALUES (?, ?, ?, ?)", user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+        new JdbcTemplate().update("INSERT INTO USERS VALUES (?, ?, ?, ?)", user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
     }
 
     public void update(User user) {
-        JdbcTemplate template = new JdbcTemplate();
-
-        template.update("update USERS set userId=?, password=?, name=?, email=? where userId=?",
+        new JdbcTemplate().update("update USERS set userId=?, password=?, name=?, email=? where userId=?",
                 user.getUserId(), user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
     }
 
     public List<User> findAll() {
-        RowMapper<User> mapper = new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet rs) throws SQLException {
-                return new User(rs.getString("userId"), rs.getString("password"),
-                        rs.getString("name"), rs.getString("email"));
-            }
-        };
-
-        return new JdbcTemplate().query("select * from USERS", mapper);
+        return new JdbcTemplate().query("select * from USERS", (rs) -> createUser(rs));
     }
 
-
     public User findByUserId(String userId) {
-        RowMapper<User> mapper = new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet rs) throws SQLException {
-                return new User(rs.getString("userId"), rs.getString("password"),
-                        rs.getString("name"), rs.getString("email"));
-            }
-        };
+        return new JdbcTemplate().queryForObject("SELECT userId, password, name, email FROM USERS WHERE userid=?",
+                (rs) -> createUser(rs), userId);
+    }
 
-        JdbcTemplate template = new JdbcTemplate();
-
-        return template.queryForObject("SELECT userId, password, name, email FROM USERS WHERE userid=?",
-                mapper, userId);
+    private User createUser(ResultSet rs) throws SQLException {
+        return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"), rs.getString("email"));
     }
 
 }
